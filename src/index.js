79d5d087b340ebb7309e0a0c603bf7893aa7b808
Viet Nguyen <>
2024-12-03 11:41:21 +0700

    MERN STACK - #16 Thiết kế database và connect tới mongodb - Lập trình thật dễ

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+console.log(process.env.PORT);
+console.log(process.env.MONGODB_URI);
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,6 +12,15 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
